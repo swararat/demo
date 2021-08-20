af33e1a846c5d5b1126ebb4296e78d59d14c5d87
swararat <89023806+swararat@users.noreply.github.com>
2021-08-20 10:52:22 +0530

    Add files via upload

---
+++ User.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+    public function expense()
+    {
+        return $this->hasMany(User::class);
+    }
+    public function ticket()
+    {
+        return $this->hasMany(User::class);
+    }
+    public function enquiry(){
+        return $this->hasMany(User::class);
+    }
+    public function followup(){
+        return $this->hasMany(User::class);
+    }
+}
